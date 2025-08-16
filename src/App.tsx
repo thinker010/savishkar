7a1678326a7c9896d9ac002abd292c0bd6ab4371
Markandeshwaragouda Patil <markandeshwaragoudapatil@gmail.com>
2025-08-16 13:43:37 +0530

    Added one moe router called the ABOUT

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Speakers from "./pages/Speakers";
+import About from "./pages/About";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
           <Route path="/speakers" element={<Speakers />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
